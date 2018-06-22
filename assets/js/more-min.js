cfa50335549618a229dfd115d20241e5df2d68a8
Adrien d'Oreye <adrien.doreye@gmail.com>
2018-06-22 13:55:24 +0200

    commit avant merge

--- assets/js/more-min.js
+++ assets/js/more-min.js
@@ -1,23 +1,32 @@
-$( "#moreMov" ).click(function(){
+$( '#moreMov' ).click(function(){
   $('.hiddenMov').fadeIn(1000);
-  $('#moreMov').addClass('hidden')
-  $('#lessMov').removeClass('hidden')
+  $('#moreMov').addClass('hidden');
+  $('#lessMov').removeClass('hidden');
 });
 
-$("#lessMov").click(function(){
+$('#lessMov').click(function(){
   $('.hiddenMov').fadeOut();
-  $('#moreMov').removeClass('hidden')
-  $('#lessMov').addClass('hidden')
+  $('#moreMov').removeClass('hidden');
+  $('#lessMov').addClass('hidden');
 })
 
-$( "#moreSer" ).click(function(){
+$( '#moreSer' ).click(function(){
   $('.hiddenSer').fadeIn(1000);
-  $('#moreSer').addClass('hidden')
-  $('#lessSer').removeClass('hidden')
+  $('#moreSer').addClass('hidden');
+  $('#lessSer').removeClass('hidden');
 });
 
-$("#lessSer").click(function(){
+$('#lessSer').click(function(){
   $('.hiddenSer').fadeOut();
-  $('#moreSer').removeClass('hidden')
-  $('#lessSer').addClass('hidden')
+  $('#moreSer').removeClass('hidden');
+  $('#lessSer').addClass('hidden');
 })
+
+
+$('#btnRight').click(function(){
+  $('.iframe1').fadeOut();
+  $('.iframe2').fadeIn();
+  $('iframe2').removeClass('hidden');
+  $('iframe1').addClass('hidden');
+
+});
