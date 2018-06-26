59232481b8c52d2033b735fed5920da7f31a8a28
Lamri Mery <Lamri.Mery@gmail.com>
2018-06-26 23:10:05 +0200

    formulaire

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,12 +1,9 @@
-//function form()
-        //{
-          
-          //  var firstname = document.getElementById("firstname").value;
-            //var lastname = document.getElementById("lastname").value;
-            //alert(lastname);
-        //}
-        function myclick(){
-            $('#btnform').click(function() {
-                alert('test');
-            });
-        }
+function myclick(){
+    var firstname = document.getElementById("firstname").value;
+    var lastname = document.getElementById("lastname").value;
+    var email = document.getElementById("email").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
+    alert(firstname + ' ' + lastname + '\n' + email + '\n' + subject + '\n' + message);
+    return false; //do not submit the form
+}
