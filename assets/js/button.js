e8947b4d03ee77bef6faa8b179b049b644edb197
Lamri Mery <Lamri.Mery@gmail.com>
2018-06-26 22:35:44 +0200

    avancé

---
+++ assets/js/button.js
@@ -0,0 +1,21 @@
+
+//var nvxButton = function(){
+    // selectionner la balise footer
+    //var myfooter = document.getElementsByTagName('footer');
+    // crée un nouvel élément button
+    //var monButton = document.createElement('button');
+    // crée un nouvel élément i
+    //var icon = document.createElement('i');
+    // ajoute l'attribut de la classe de fontawesome
+    //icon.setAttribute('class', 'far fa-arrow-alt-circle-up');
+    // ajoute le noeud texte au nouveau div créé
+    //monButton.appendChild(icon);
+    //console.log('ici est mon' + typeof(monButton));
+    //docume
+    //$(monButton).onclick(function(){
+      //$(widows ).onscroll;
+    
+    //});
+//};
+
+
---
+++ assets/js/form.js
@@ -0,0 +1,12 @@
+//function form()
+        //{
+          
+          //  var firstname = document.getElementById("firstname").value;
+            //var lastname = document.getElementById("lastname").value;
+            //alert(lastname);
+        //}
+        function myclick(){
+            $('#btnform').click(function() {
+                alert('test');
+            });
+        }
