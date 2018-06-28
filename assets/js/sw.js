f4be631dc26ff6a27b25548f126dc4d97081133e
Adrien d'Oreye <adrien.doreye@gmail.com>
2018-06-28 09:17:32 +0200

    sw.js

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,7 +1,13 @@
 self.addEventListener('install', function(event) {
   // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
-
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
   '/assets/css/main.css',
@@ -32,16 +38,7 @@ var urlsToCache = [
   '/assets/js/series.json',
 ];
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
+
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
