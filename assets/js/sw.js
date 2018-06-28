d2fc61f5e575b20520c788885c6c020b348fd637
Adrien d'Oreye <adrien.doreye@gmail.com>
2018-06-28 09:23:53 +0200

    sw.js

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -10,35 +10,35 @@ self.addEventListener('install', function(event) {
 });
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
-  '/assets/css/main.css',
-  '/assets/css/menu.css',
-  '/assets/css/style.css',
-  '/assets/img/alien-banner.jpg',
-  '/assets/img/alien.jpeg',
-  '/assets/img/batmanmovie-2017-comedie.jpg',
-  '/assets/img/hostel-2005-thriller.jpg',
-  '/assets/img/intouchables-2011-comedie.jpg',
-  '/assets/img/lesdeuxtours-2002-aventure.jpg',
-  '/assets/img/inception-2010-scifi.jpg',
-  '/assets/img/thefall-2006-dramatique.jpg',
-  '/assets/img/shutterisland-2010-thriller.jpg',
-  '/assets/img/storks.jpeg',
-  '/assets/img/lepatientanglais-1996-dramatique.jpg',
-  '/assets/img/seven-1995-thriller.jpg',
-  '/assets/img/Solo.jpg',
-  '/assets/img/starwarsempire-1980-scifi.jpg',
-  '/assets/img/Survivestyle5-2004-comedie.jpg',
-  '/assets/img/swissarmyman-2016-comedie.jpg',
-  '/assets/img/WhatWeDoInTheShadows-2014-comedie.jpg',
-  '/assets/img/zoolander-2001-comedie.jpg',
-  '/assets/js/btton.js',
-  '/assets/js/form.js',
-  '/assets/js/main.js',
-  '/assets/js/movies.json',
-  '/assets/js/series.json',
+  '../css/main.css',
+  '../css/menu.css',
+  '../css/style.css',
+  '../img/alien-banner.jpg',
+  '../img/alien.jpeg',
+  '../img/batmanmovie-2017-comedie.jpg',
+  '../img/hostel-2005-thriller.jpg',
+  '../img/intouchables-2011-comedie.jpg',
+  '../img/lesdeuxtours-2002-aventure.jpg',
+  '../img/inception-2010-scifi.jpg',
+  '../img/thefall-2006-dramatique.jpg',
+  '../img/shutterisland-2010-thriller.jpg',
+  '../img/storks.jpeg',
+  '../img/lepatientanglais-1996-dramatique.jpg',
+  '../img/seven-1995-thriller.jpg',
+  '../img/Solo.jpg',
+  '../img/starwarsempire-1980-scifi.jpg',
+  '../img/Survivestyle5-2004-comedie.jpg',
+  '../img/swissarmyman-2016-comedie.jpg',
+  '../img/WhatWeDoInTheShadows-2014-comedie.jpg',
+  '../img/zoolander-2001-comedie.jpg',
+  '../js/btton.js',
+  '../js/form.js',
+  '../js/main.js',
+  '../js/movies.json',
+  '../js/series.json',
 ];
-
-
+console.log(urlsToCache);
+console.log('test url');
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
